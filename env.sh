2fd0ed96b96c93a81bf9c649a561ae7121c12754
Michael Noukhovitch <michael.noukhovitch@umontreal.ca>
2017-09-14 15:20:34 -0400

    fix fetch.sh, add env

---
+++ env.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+
+export PROJECT_DIR=$HOME/common_sense_prediction
+export PYTHONPATH=$PYTHONPATH:PROJECT_DIR
+export DATA_DIR=/data/lisatmp4/common_sense_prediction
+#export ACL_ROOT_DIR=$HOME/l2lwe/ACL_CKBC
+#export PYTHONPATH=$PYTHONPATH:$ACL_ROOT_DIR
+
