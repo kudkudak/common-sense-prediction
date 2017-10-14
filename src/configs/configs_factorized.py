85f050b86eaf29390dd86df1df0b31c230370c4e
Michael Noukhovitch <michael.noukhovitch@umontreal.ca>
2017-10-13 23:39:53 -0400

    updated env and configs

--- src/configs/configs_factorized.py
+++ src/configs/configs_factorized.py
@@ -8,15 +8,12 @@ from src.utils.vegab import ConfigRegistry
 config = ConfigRegistry()
 
 config.set_root_config({
-    'batch_size': 600,
+    'batch_size': 200,
     'epochs': 30,
-    'rel_init': 0.02,
-    'rel_vec_size': 200,
     'activation': 'linear',
-    'hidden_units': 500,
-    'l2': 0.00001, # "cost_new = (1000*loss) +(self.LC * l2_penalty1)" from original code ;)
+    'hidden_units': 150,
+    'l2': 0.001, # "cost_new = (1000*loss) +(self.LC * l2_penalty1)" from original code ;)
     # 'lambda_2': 0.0, # Matrix for relation matrix # No identity matrix in DNN CE
     'learning_rate': 0.01,
     'embedding_file': 'embeddings/LiACL/embeddings_OMCS.txt',
 })
-
