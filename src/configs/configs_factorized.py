cbecc3edd4c0a29cce50ee471d3a90a3b97e6196
Michael Noukhovitch <mnoukhov@gmail.com>
2017-10-16 13:04:01 -0400

    improved factorized config

--- src/configs/configs_factorized.py
+++ src/configs/configs_factorized.py
@@ -10,10 +10,10 @@ config = ConfigRegistry()
 config.set_root_config({
     'batch_size': 200,
     'epochs': 30,
-    'activation': 'tanh',
+    'activation': 'relu',
     'hidden_units': 150,
     'rel_init': 0.02,
-    'l2': 0.001, # "cost_new = (1000*loss) +(self.LC * l2_penalty1)" from original code ;)
+    'l2': 1e-6, # "cost_new = (1000*loss) +(self.LC * l2_penalty1)" from original code ;)
     # 'lambda_2': 0.0, # Matrix for relation matrix # No identity matrix in DNN CE
     'learning_rate': 0.01,
     'embedding_file': 'embeddings/LiACL/embeddings_OMCS.txt',
