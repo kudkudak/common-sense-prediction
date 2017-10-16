395180b2f99bb2dd166f8fba235c72c2c167ac31
Michael Noukhovitch <mnoukhov@gmail.com>
2017-10-15 20:34:21 -0400

    dnn ce should be equivalent to paper

--- src/configs/configs_dnn_ce.py
+++ src/configs/configs_dnn_ce.py
@@ -14,7 +14,7 @@ config.set_root_config({
     'rel_vec_size': 200,
     'activation': 'relu',
     'hidden_units': 1000,
-    'l2': 0.00001, # "cost_new = (1000*loss) +(self.LC * l2_penalty1)" from original code ;)
+    'l2': 0.001, # "cost_new = (1000*loss) +(self.LC * l2_penalty1)" from original code ;)
     # 'lambda_2': 0.0, # Matrix for relation matrix # No identity matrix in DNN CE
     'learning_rate': 0.01,
     'embedding_file': 'embeddings/LiACL/embeddings_OMCS.txt',
