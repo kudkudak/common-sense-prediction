89dd3cadb1624c98df73f35da7d9880fecf7c0b3
Michael Noukhovitch <mnoukhov@gmail.com>
2019-01-28 15:36:14 -0500

    environments and tuples cleanup
    cleaned up env creation
    made conda envs py2 + py3 the same
    created requirements.txt for pip
    removed `tuples.wiki` from unrelated spots
    fixed `fetch_LiACL_data.py` tiny bugs
    added cleanup to `fetch_and_split_extrinsic_LiACL.py`

--- env.sh
+++ env.sh
@@ -1,10 +1,7 @@
 #!/usr/bin/bash
 
 source activate csp
-export PROJECT_DIR=$HOME/common_sense_prediction
+export PROJECT_DIR=$HOME/common-sense-prediction
 export PYTHONPATH=$PYTHONPATH:$PROJECT_DIR
-export DATA_DIR=/data/lisatmp4/common_sense_prediction
-#export DATA_DIR=$PROJECT_DIR/data
-#export ACL_ROOT_DIR=$HOME/l2lwe/ACL_CKBC
-#export PYTHONPATH=$PYTHONPATH:$ACL_ROOT_DIR
+export DATA_DIR=$PROJECT_DIR/data
 
--- scripts/data/fetch_LiACL_data.sh
+++ scripts/data/fetch_LiACL_data.sh
@@ -1,8 +1,19 @@
 #!/usr/bin/env bash
 
-# Fetches data used in http://ttic.uchicago.edu/~kgimpel/papers/li+etal.acl16.pdf
-
-# Execute this script in data folder
+# Fetches training data used in Commonsense Knowledge Base Completion (Li et al 2016)
+# Execute this script in data folder ($DATA_DIR) to create
+# LiACL\
+#   dev1.txt
+#   dev2.txt
+#   rel.txt
+#   rel_lowercase.txt
+#   test.txt
+#   train100k.txt
+#   train300k.txt
+#   train600k.txt
+# embeddings\
+#   LiACL\
+#       embeddings_OMCS.txt
 
 set -x
 set -e
@@ -18,7 +29,7 @@ wget http://ttic.uchicago.edu/~kgimpel/comsense_resources/dev2.txt.gz
 wget http://ttic.uchicago.edu/~kgimpel/comsense_resources/test.txt.gz
 wget http://ttic.uchicago.edu/~kgimpel/comsense_resources/embeddings.txt.gz
 
-wget https://gist.github.com/mnuke/86d8a02c20b24f7f58a50db415babcdf/raw/b5c2d2d70e2bd091ea03ed799b28e041e78c94ed/rel.txt
+wget https://gist.github.com/mnoukhov/86d8a02c20b24f7f58a50db415babcdf/raw/b5c2d2d70e2bd091ea03ed799b28e041e78c94ed/rel.txt
 
 gunzip train100k.txt.gz
 gunzip train300k.txt.gz
@@ -28,7 +39,6 @@ gunzip dev2.txt.gz
 gunzip test.txt.gz
 gunzip embeddings.txt.gz
 
-mv tuples.wiki LiACL
 mv train100k.txt train300k.txt train600k.txt dev1.txt dev2.txt test.txt rel.txt LiACL/conceptnet
 mv embeddings.txt embeddings/LiACL/embeddings_OMCS.txt
 
